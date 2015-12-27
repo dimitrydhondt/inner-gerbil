ee9f5aac70b08d74bd3fa940f224b1e6069b81bc
Bernard Butaye <bernardenmartine@gmail.com>
2015-12-28 00:12:36 +0100

    elas import: refactoring + add messages import to launcher

--- elas-import/import.js
+++ elas-import/import.js
@@ -1,3 +1,16 @@
-var PATH_TO_FILE = 'elas-users-2015-10-14.csv';
-var elasImporter = require('./importUsers.js');
-elasImporter(process.cwd() + '/' + PATH_TO_FILE);
+var PATH_TO_FILE = 'data/users_2015-12-27.csv';
+var PATH_TO_MSGS_FILE = 'data/messages_2015-12-27.csv'
+var importer = require('./importer.js');
+var importUsers = require('./importUsers.js');
+var importUser = importUsers.addUserToParty;
+var importMessage = require('./importMessages.js');
+
+hrefs = {
+  PARTY_LETSDENDERMONDE: '/parties/8bf649b4-c50a-4ee9-9b02-877aa0a71849'
+}
+importer(process.cwd() + '/' + PATH_TO_FILE, function (user) {
+  return importUser(user, hrefs.PARTY_LETSDENDERMONDE);
+});
+importer(process.cwd() + '/' + PATH_TO_MSGS_FILE, function (message) {
+  return importMessage(message, hrefs.PARTY_LETSDENDERMONDE);
+});
--- elas-import/importUsers.js
+++ elas-import/importUsers.js
@@ -5,86 +5,131 @@ var port = 5000;
 var base = 'http://localhost:' + port;
 
 var sriclient = require('sri4node-client');
+var doGet = sriclient.get;
 var doPut = sriclient.put;
 
-exports = module.exports = function (user, partyUrl) {
-  'use strict';
-  var uuid = common.generateUUID();
-  var convUserStatusToPartyStatus = function (status) {
-    switch (status) {
-    case 0: // inactief
-      return 'inactive';
-    case 1: // actieve letser
-      return 'active';
-    case 2: // uitstapper
-      return 'active';
-    case 3: // instapper
-      return 'active';
-    case 4: // secretariaat
-      return 'active';
-    case 5: // infopakket
-      return 'inactive';
-    case 6: // instap gevolgd
-      return 'inactive';
-    case 7: // extern
-      return 'active';
-    default:
-      return 'inactive';
-    }
-  };
-  var convElasAccountroleToPartyrelType = function (accountrole) {
-    if (accountrole === 'user') {
-      return 'member';
-    } else if (accountrole === 'admin') {
-      return 'administrator';
-    }
-  };
-  var party = {
-    type: 'person',
-    name: user.name,
-    alias: user.letscode.toString(),
-    login: user.login,
-    password: user.password,
-    status: convUserStatusToPartyStatus(user.status)
-  };
-  console.log(party);
-  var partyrelation = {
-    from: {
-      href: '/parties/' + uuid
-    },
-    to: {
-      href: partyUrl
-    },
-    type: convElasAccountroleToPartyrelType(user.accountrole),
-    balance: 0,
-    code: user.letscode.toString(),
-    status: convUserStatusToPartyStatus(user.status)
+exports = module.exports = {
+  addUserToGroup: function (user, groupAlias) {
+    'use strict';
 
-  };
-  console.log(partyrelation);
-  var batchBody = [
-    {
-      href: '/parties/' + uuid,
-      verb: 'PUT',
-      body: party
+    function debug(x) {
+      console.log(x); // eslint-disable-line
+    }
+    var createGroup = function (groupAlias) {
+      var group = {
+        type: 'group',
+        name: groupAlias,
+        alias: groupAlias,
+        status: 'inactive'
+      };
+      var groupHref = '/parties/' + common.generateUUID();
+      return doPut(base + groupHref, group, 'annadv', 'test').then(function (
+        createGroupResponse) {
+        if (createGroupResponse.statusCode !== 200 && createGroupResponse.statusCode !== 201) {
+          debug('Error in creation of group: ' + createGroupResponse.statusCode);
+          debug(createGroupResponse.body);
+          throw Error('Unable to create group with alias ' + groupAlias);
+        };
+        debug(createGroupResponse.body);
+        return groupHref;
+      });
+    };
+    return doGet(base + '/parties?alias=' + groupAlias, 'annadv', 'test').then(function (
+      responseParty) {
+      if (responseParty.statusCode !== 200) {
+        debug('Error in get parties: ' + responseParty.statusCode);
+      }
+      var parties = responseParty.body;
+      if (parties.$$meta.count === 0) {
+        debug('No parties with alias ' + groupAlias + ' found !!');
+        debug(responseParty.body);
+        return createGroup(groupAlias);
+      } else {
+        return parties.results[0].href;
+      }
+    }).then(function (partyHref) {
+      return exports.addUserToParty(user, partyHref);
+    });
+  },
+  addUserToParty: function (user, partyUrl) {
+    'use strict';
+    var uuid = common.generateUUID();
+    var convUserStatusToPartyStatus = function (status) {
+      switch (status) {
+      case 0: // inactief
+        return 'inactive';
+      case 1: // actieve letser
+        return 'active';
+      case 2: // uitstapper
+        return 'active';
+      case 3: // instapper
+        return 'active';
+      case 4: // secretariaat
+        return 'active';
+      case 5: // infopakket
+        return 'inactive';
+      case 6: // instap gevolgd
+        return 'inactive';
+      case 7: // extern
+        return 'active';
+      default:
+        return 'inactive';
+      }
+    };
+    var convElasAccountroleToPartyrelType = function (accountrole) {
+      if (accountrole === 'user') {
+        return 'member';
+      } else if (accountrole === 'admin') {
+        return 'administrator';
+      }
+    };
+    var party = {
+      type: 'person',
+      name: user.name,
+      alias: user.letscode.toString(),
+      login: user.login,
+      password: user.password,
+      status: convUserStatusToPartyStatus(user.status)
+    };
+    console.log(party);
+    var partyrelation = {
+      from: {
+        href: '/parties/' + uuid
+      },
+      to: {
+        href: partyUrl
+      },
+      type: convElasAccountroleToPartyrelType(user.accountrole),
+      balance: 0,
+      code: user.letscode.toString(),
+      status: convUserStatusToPartyStatus(user.status)
+
+    };
+    console.log(partyrelation);
+    var batchBody = [
+      {
+        href: '/parties/' + uuid,
+        verb: 'PUT',
+        body: party
           },
-    {
-      href: '/partyrelations/' + common.generateUUID(),
-      verb: 'PUT',
-      body: partyrelation
+      {
+        href: '/partyrelations/' + common.generateUUID(),
+        verb: 'PUT',
+        body: partyrelation
           }
     ];
 
-  return doPut(base + '/batch', batchBody, 'annadv', 'test').then(function (
-    response) {
-    if (response.statusCode !== 200 && response.statusCode !== 201) {
-      console.log('PUT failed, response = ' + JSON.stringify(response));
-    } else {
-      console.log('PUT successful');
-    }
-  }).catch(function (e) {
-    console.log('importUser failed');
-    console.log(e);
-    throw e;
-  });
+    return doPut(base + '/batch', batchBody, 'annadv', 'test').then(function (
+      response) {
+      if (response.statusCode !== 200 && response.statusCode !== 201) {
+        console.log('PUT failed, response = ' + JSON.stringify(response));
+      } else {
+        console.log('PUT successful');
+      }
+    }).catch(function (e) {
+      console.log('importUser failed');
+      console.log(e);
+      throw e;
+    });
+  }
 };
