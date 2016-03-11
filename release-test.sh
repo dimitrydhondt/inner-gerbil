1ae160308ae9ba30a162024760ce548767b6d3c0
Dimitry D'hondt <dimitry_dhondt@yahoo.com>
2016-03-11 12:47:13 +0000

    set profile images after init of test database

--- release-test.sh
+++ release-test.sh
@@ -3,3 +3,4 @@ cat sql/clean-database.sql | heroku pg:psql
 cat sql/schema.sql | heroku pg:psql
 cat sql/testdata.sql | heroku pg:psql
 git push heroku master
+curl -X PUT -u annadv:test -F 'filedata=@/home/ubuntu/workspace/inner-gerbil/test/orange-boy-icon.png' https://inner-gerbil-test.herokuapp.com/parties/5df52f9f-e51f-4942-a810-1496c51e6/profile.png
